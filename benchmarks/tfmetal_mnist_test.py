45ba5e5168288b98bf7b181524644b9d101e27c1
Peter Lin <woolfel@gmail.com>
2021-11-03 11:36:43 -0400

    adding a different version that doesn't use mlcompute

---
+++ benchmarks/tfmetal_mnist_test.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+print(tf.__version__)
+
+# the benchmark loads the MNIST dataset from tensorflow datasets
+# a possible alternative is fashion MNIST, which should require more power
+(ds_train, ds_test), ds_info = tfds.load(
+    'fashion_mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+
+def normalize_img(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
+
+# you can change the batch size to see how it performs. Larger batch size will stress GPU more
+batch_size = 128
+
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(batch_size)
+ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+
+
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_test = ds_test.batch(batch_size)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(32, kernel_size=(3, 3),
+                 activation='relu'),
+  tf.keras.layers.Conv2D(64, kernel_size=(3, 3),
+                 activation='relu'),
+  tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+#   tf.keras.layers.Dropout(0.25),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+#   tf.keras.layers.Dropout(0.5),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    metrics=['accuracy'],
+)
+
+model.fit(
+    ds_train,
+    epochs=12,
+    validation_data=ds_test,
+)
